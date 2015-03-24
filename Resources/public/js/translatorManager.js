b973c70c30fbc621d06f6182dcd731c6adf7cdc9
Rodrigo Santellan <rsantellan@gmail.com>
2015-03-24 17:57:34 -0300

    Updated tinymce.

--- Resources/public/js/translatorManager.js
+++ Resources/public/js/translatorManager.js
@@ -165,12 +165,16 @@ translatorManager.prototype = {
           force_p_newlines: false,
           force_br_newlines : false,
           forced_root_block : '',
+          theme: "modern",
           plugins: [
-              "advlist autolink lists link image charmap print preview anchor",
-              "searchreplace visualblocks code fullscreen",
-              "insertdatetime media table contextmenu paste"
+              "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+              "searchreplace wordcount visualblocks visualchars code fullscreen",
+              "insertdatetime media nonbreaking save table contextmenu directionality",
+              "emoticons template paste textcolor colorpicker textpattern"
           ],
-          toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+          toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+          toolbar2: "forecolor backcolor | fontselect | fontsizeselect"
+
       };
       //console.info(tinyOptions);
       $('#'+key+'_translation textarea').tinymce(tinyOptions);
